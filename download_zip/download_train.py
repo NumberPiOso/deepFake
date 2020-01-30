a38184444bbc92be910a2526540cd586b5f95ee8
Pablo <osoriopabl@gmail.com>
2020-01-30 13:56:26 -0500

    deleted folder for organization

--- download_zip/download_train.py
+++
@@ -1,6 +0,0 @@
-import requests, zipfile, io
-
-url = 'https://github.com/NumberPiOso/deepFake/raw/master/read.zip'
-req = requests.get(url)
-file = zipfile.ZipFile(io.BytesIO(req.content))
-file.extractall()
