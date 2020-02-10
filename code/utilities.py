c92e80efb4369a0d7e1d5e087b2369310b49e7c5
Pablo <osoriopabl@gmail.com>
2020-02-10 10:27:36 -0500

    found a way to get every frame of the video and a way to encode this faces in order to trying to detect differences

--- code/utilities.py
+++ code/utilities.py
@@ -1,1 +1,22 @@
+import face_recognition
+
+def crop_faces(image):
+    '''
+    Receives a images and returns the cropped of the face in the image.
+    Inputs:
+        image (array of ints) usually np.ndarray
+            dim n1 x n2 x n3 where n3 usually represents the differents colors.
+    Output:
+        status (bool):
+            Ok if a face was found.
+        cropped_face (array of ints)
+            subarrays of Image if face was found.
+    '''
+    face_locations = face_recognition.face_locations(image)
+    faces_imgs = []
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+        face_image = image[top:bottom, left:right]
+        faces_imgs.append(face_image)
+    return faces_imgs
 
